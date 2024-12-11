853b83f8d52d54c2ab23ee2d3259596930369237
kim-hoyoung <happy3sk@naver.com>
2024-12-11 21:41:53 +0900

    add: 코드 리뷰 폴더 생성

---
+++ review/main.js
@@ -0,0 +1,9 @@
+let espresso = 10;
+let milk = 170;
+let chocolateSyru = 50;
+let whippedCream = 60;
+
+console.log(espresso);
+console.log(espresso + milk);
+console.log(espresso + chocolateSyru + milk);
+console.log(espresso + chocolateSyru + milk + whippedCream);
