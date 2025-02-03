e1b518d20067b7dc23cdf325c6b2116b3af83831
kim-hoyoung <happy3sk@naver.com>
2025-02-03 18:13:19 +0900

    docs: state와 Props 실습

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
-import Button from "./components/Button.jsx";
+
+import Register from "./components/Register";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-  //부모 컴포넌트
   return (
     <>
-      <Header /> {/* 자식 컴포넌트 */}
-      <Main />
-      <Footer />
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <Register />
     </>
   );
 }
