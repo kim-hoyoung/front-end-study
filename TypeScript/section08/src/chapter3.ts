113df57d5aacf49a1b31d18a7ef5e5006228796f
김호영 <happy3sk@naver.com>
2025-03-07 22:42:07 +0900

    docs: study indexed Access Types
    docs: keyof 연산자에 대한 study
    
    docs: mappedType 에 대한 study
    
    docs: 템플릿 리터럴 타입에 대한 study

---
+++ TypeScript/section08/src/chapter3.ts
@@ -0,0 +1,43 @@
+/**
+ * 맵드 타입 : 인터페이스에서는 사용 불가능
+ */
+
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
+type PartialUser = {
+  [key in "id" | "name" | "age"]?: User[key];
+};
+type BooleanUser = {
+  [key in keyof User]: boolean;
+};
+
+type ReadonlyUser = {
+  readonly [key in keyof User]: User[key];
+};
+
+// 한 명의 유저 정보를 불러오는 기능
+function fetchUser(): ReadonlyUser {
+  //....기능
+  return {
+    id: 1,
+    name: "홍길동",
+    age: 27,
+  };
+}
+const user = fetchUser();
+//user.id = 1;
+
+// 한명의 유저 정보를 수정하는 기능
+function updateUser(user: PartialUser) {
+  //.. 수정하는 기능
+}
+
+updateUser({
+  // id: 1,
+  // name: "홍길동",
+  age: 25,
+});
