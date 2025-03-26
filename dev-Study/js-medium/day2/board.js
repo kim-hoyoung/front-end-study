ac1cb2de9a8b71bd98822773d520c3bab6af8fdd
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-03-26 17:45:04 +0900

    Hoyoung (#62)
    * ? (#61)
    
    * Delete groomStudy/js-medium directory
    
    * docs: dev-Study 파일 추가 (#58)
    
    * docs: dev-Study 파일 추가 (#59)
    
    * docs: dev-Study 파일 추가 (#59)
    
    * Update README.md
    
    디테일 추가
    
    * docs: this, map, reduce, Math 함수 응용 내용 정리.

--- dev-Study/js-medium/day2/board.js
+++ dev-Study/js-medium/day2/board.js
@@ -1,11 +1,24 @@
 const board = {
   posts: [],
+
   addPost(content) {
-    this.posts = push(content);
-    console.log(`게시글 추가 : ${content}`);
+    this.posts.push(content);
+    console.log(`게시글 추가: ${content}`);
   },
-  showPosts() {},
-  removePost() {},
+
+  removePost(index) {
+    if (index < 1 || index > this.posts.length) {
+      console.log("삭제할 게시글이 없습니다.");
+    }
+    const removed = this.posts.splice(index - 1, 1);
+    console.log(`삭제된 게시글: ${removed[0]}`);
+  },
+  showPosts() {
+    console.log("현재 게시글 목록:");
+    this.posts.forEach((post, idx) => {
+      console.log(`${idx + 1}.${post}`);
+    });
+  },
 };
 
 // 실행 예시
@@ -18,7 +31,7 @@ board.showPosts();
 // 1. 오늘의 점심은 김치찌개!
 // 2. 자바스크립트 공부 중 🖥️
 
-board.removePost(1);
+board.removePost(4);
 // 출력:
 // 게시글 삭제: 오늘의 점심은 김치찌개!
 
@@ -26,4 +39,3 @@ board.showPosts();
 // 출력:
 // 📌 현재 게시글 목록:
 // 1. 자바스크립트 공부 중 🖥️
-// 변경사항 확인
