f85fe07f3e6f3326f94fdc8ecb9ca9994fb172f3
kim-hoyoung <happy3sk@naver.com>
2025-03-31 13:04:20 +0900

    docs: react 실습 초기 세팅

---
+++ dev-Study/first-react-app/src/components/Bio.jsx
@@ -0,0 +1,16 @@
+export default function Bio() {
+  return (
+    <>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br />
+        <strong>
+          <em>And pictures of scientists!</em>
+        </strong>
+      </p>
+    </>
+  );
+}
