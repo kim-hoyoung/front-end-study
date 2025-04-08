901852e3ac316864befed33eee558340bce0b22c
kim-hoyoung <happy3sk@naver.com>
2025-04-08 17:46:05 +0900

    docs: useState ShopItemList 코드 주석 추가

--- dev-Study/first-react-app/src/components/ShopItemList.jsx
+++ dev-Study/first-react-app/src/components/ShopItemList.jsx
@@ -1,12 +1,13 @@
 import "./ShopItemList.css";
 
 const ShopItemList = ({ data, onIncreaseCartCount }) => {
+  // 구조분해할당으로 App에서 data와 onIncreaseCartCount를 변수명으로 받아옴
   return (
     <li>
       <p>{data.productName}</p>
       <p>{data.productPrice}</p>
       <span>장바구니 수량 : {data.count}</span>
-      <button onClick={() => onIncreaseCartCount(data.id)}>장바구니 +1</button>
+      <button onClick={() => onIncreaseCartCount(data.id)}>장바구니 + 1</button>
     </li>
   );
 };
