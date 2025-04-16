8b0f7eaf5c14f1872a25d44024bdbd6fa69a4470
kim-hoyoung <happy3sk@naver.com>
2025-04-16 13:02:08 +0900

    style: design 변경

--- dev-Study/memo-app/src/App.jsx
+++ dev-Study/memo-app/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useCallback, useState } from "react";
+import SideBar from "./components/SideBar";
+import MemoContainer from "./components/MemoContainer";
+import localStorageSetItem from "./utils/storage";
 
+function initMemosGetItem() {
+  return JSON.parse(localStorage.getItem("memos")) || [];
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [memos, setMemos] = useState(initMemosGetItem);
+  const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
+
+  const handleMemoChange = useCallback(
+    (newMemo) => {
+      setMemos((prevMemos) => {
+        const newMemos = [...prevMemos]; // 오타 수정
+        newMemos[selectedMemoIndex] = newMemo;
+        localStorageSetItem("memos", newMemos); // 로컬 저장
+        return newMemos;
+      });
+    },
+    [selectedMemoIndex]
+  );
+  const handleAddMemo = useCallback(() => {
+    const now = new Date().getTime();
+    const newMemo = {
+      title: "Untitled",
+      content: "",
+      createAt: now,
+      updateAt: now,
+    };
+    setMemos([...memos, newMemo]);
+    setSelectedMemoIndex(memos.length);
+
+    localStorageSetItem("memos", [...memos, newMemo]);
+  }, [memos]);
+
+  const hadleDeleteMemo = useCallback(
+    (deleteMemoIndex) => {
+      const newMemo = memos.filter((memo, index) => index !== deleteMemoIndex);
+      setMemos(newMemo);
+
+      localStorageSetItem("memos", newMemo);
+
+      if (deleteMemoIndex === selectedMemoIndex) {
+        setSelectedMemoIndex(0);
+      } else if (deleteMemoIndex < selectedMemoIndex) {
+        setSelectedMemoIndex(selectedMemoIndex - 1);
+      }
+    },
+    [memos, selectedMemoIndex]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <SideBar
+        memos={memos}
+        selectedMemoIndex={selectedMemoIndex}
+        setSelectedMemoIndex={setSelectedMemoIndex}
+        onAddMemo={handleAddMemo}
+        onMemoDeleteClick={hadleDeleteMemo}
+      />
+      <MemoContainer
+        memo={memos[selectedMemoIndex]}
+        onMemoChange={handleMemoChange}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
