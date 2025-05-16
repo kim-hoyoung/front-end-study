842b53707a0147cea984f94d2fed079d5b3f1a0d
김호영 <happy3sk@naver.com>
2025-05-16 15:43:31 +0900

    docs: javaScript object 학습

--- frontend(hmtl,css,js)/html_css_js/javaScript/object.js
+++ frontend(hmtl,css,js)/html_css_js/javaScript/object.js
@@ -11,5 +11,81 @@ const objectName = {
   },
 };
 
-console.log(objectName.name);
-console.log(objectName.get_data());
+// console.log(objectName.name);
+// console.log(objectName.get_data());
+
+const user = {
+  age: 10,
+  name: "Dave",
+  details: {
+    hobby: "coding",
+    major: "korean",
+    get_hobby: () => this.hobby, //this문법은 화살표 함수에서 쓰지 않는게 좋아용
+  },
+};
+
+// console.log(user.details.get_hobby());
+
+// getter setter
+
+const user1 = {
+  age: 10,
+  name: "Dave Lee",
+
+  get get_age() {
+    return this.age;
+  },
+  set set_age(value) {
+    this.age = value;
+  },
+};
+
+console.log(user1.get_age); // 내부 프로퍼티를 직접 건드는걸 지양 하기 위해, getter, setter를 사용한다.
+user1.set_age = 20;
+console.log(user1.get_age);
+
+console.log("----------------------");
+
+// 2. new Object() 로 생성하는 방식 (거의 안쓰이긴 해)
+
+const user2 = new Object();
+
+user2.age = 10;
+user2.name = "Kim";
+user2.get_data = function () {
+  return 1 + 2;
+};
+
+console.log(user2.age, user2.name, user2.get_data());
+console.log("----------------------");
+
+// 3. 생성자 함수로 생성하는 방식
+
+function User3(age, name) {
+  this.age = age;
+  this.name = name;
+  this.get_data = function () {
+    return this.age;
+  };
+}
+
+const dave = new User3(10, "Dave");
+console.log(typeof dave);
+console.log(dave.age, dave.name, dave.get_data());
+
+// 프로토타입
+// 생성자 함수에 프로퍼티 또는 메서드 정의가 가능.
+
+function User4(age, name) {
+  this.age = age;
+  this.name = name;
+}
+
+User4.prototype.message = function () {
+  return "Hello";
+};
+
+User4.prototype.hobby = "coding";
+
+const ho = new User4(10, "Ho");
+console.log(ho.age, ho.name, ho.hobby, ho.message());
